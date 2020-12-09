8292bfdd6b4472fd6539b6bac9f36facaaa64d2f
calshiyi <m1738837619@163.com>
2020-12-09 16:14:59 +0800

    remove target log and some other config

--- cainiao-kafka/src/main/java/com/cainiao/kafka/producer/KafkaProducer.java
+++ cainiao-kafka/src/main/java/com/cainiao/kafka/producer/KafkaProducer.java
@@ -1,6 +1,5 @@
 package com.cainiao.kafka.producer;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
